84634a1ba467776371d8d575f3708d0eaa28110d
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-29 14:41:18 -0300

    Validando usuário administrador.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = (app) => {
   app.post('/signup', app.api.user.save)
   app.post('/signin', app.api.auth.signin)
@@ -5,18 +7,18 @@ module.exports = (app) => {
 
   app.route('/users')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
-    .post(app.api.user.save) // ação será feita pelo consign para registrar novo usuário
-    .get(app.api.user.get) // ação para buscar dados de usuários
+    .post(admin(app.api.user.save)) // ação será feita pelo consign para registrar novo usuário se for admin
+    .get(admin(app.api.user.get)) // ação para buscar dados de usuários se for admin
 
   app.route('/users/:id')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
-    .put(app.api.user.save) // ação para atualizar usuário
-    .get(app.api.user.getById) // ação para buscar usuário específico por ID
+    .put(admin(app.api.user.save)) // ação para atualizar usuário se for admin
+    .get(admin(app.api.user.getById)) // ação para buscar usuário específico por ID se for admin
 
   app.route('/categories')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
-    .get(app.api.category.get) // busca todas as categorias
-    .post(app.api.category.save) // registra nova categoria
+    .get(admin(app.api.category.get)) // busca todas as categorias se for admin
+    .post(admin(app.api.category.save)) // registra nova categoria se for admin
 
   // Tem que vir anter de categories/:id
   app.route('/categories/tree')
@@ -26,19 +28,19 @@ module.exports = (app) => {
   app.route('/categories/:id')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
     .get(app.api.category.getById) // busca categoria por ID
-    .put(app.api.category.save) // atualiza categoria
-    .delete(app.api.category.remove) // remove uma categoria
+    .put(admin(app.api.category.save)) // atualiza categoria se for admin
+    .delete(admin(app.api.category.remove)) // remove uma categoria se for admin
 
   app.route('/articles')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
-    .get(app.api.article.get) // busca todos os artigos
-    .post(app.api.article.save) // registra um novo artigo
+    .get(admin(app.api.article.get)) // busca todos os artigos se for admin
+    .post(admin(app.api.article.save)) // registra um novo artigo se for admin
 
   app.route('/articles/:id')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
     .get(app.api.article.getById) // busca artigo por ID
-    .put(app.api.article.save) // atualiza artigo
-    .delete(app.api.article.remove) // remove um artigo
+    .put(admin(app.api.article.save)) // atualiza artigo se for admin
+    .delete(admin(app.api.article.remove)) // remove um artigo se for admin
 
   app.route('/categories/:id/articles')
     .all(app.config.passport.authenticate()) // só permite a execução dos outros métodos se passar pela autenticação
