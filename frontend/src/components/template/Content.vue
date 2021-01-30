2957f4562538c21505fa820f1cd5819ed3f8ce7e
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-30 17:21:36 -0300

    Rotas.

--- frontend/src/components/template/Content.vue
+++ frontend/src/components/template/Content.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="content"></div>
+  <div class="content">
+    <router-view></router-view> <!-- Rederiza em Content o componente selecionado na rota -->
+  </div>
 </template>
 
 <script>
