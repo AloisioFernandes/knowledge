324d8d21fe14df7bd251dbb9ed7483c49fc5b8f9
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-01-31 16:12:00 -0300

    Cadastro de usuário por administrador.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
 // TEMPORÁRIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NywibmFtZSI6Ikpvw6NvIiwiZW1haWwiOiJqb2FvQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjEyMDQwOTY2LCJleHAiOjE2MTIzMDAxNjZ9.g_0Tvmonaj94ZKcZ2_c1idTEfq0yvBwhKj82VIzuRO4'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsb8Otc2lvIEZlcm5hbmRlcyBHLiIsImVtYWlsIjoiYWxvaXNpby5mZXJuYW5kZXNAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxMjEwOTkxMywiZXhwIjoxNjEyMzY5MTEzfQ.AC1sDa6iKg5-l8ei0IjNvvjW87JKPGzVwXpxHLrGP38'
 
 new Vue({
   store, // store pode ser compartilhada entre os componentes
